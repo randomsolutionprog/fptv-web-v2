d1d0cd4183edb746b44e593a51fff44bd3549678
Tengku Muhammad Muzaffar Bin Tengku Masly Mohd Gadafi <randomsolutionprog@gmail.com>
2024-05-21 15:56:14 +0800

    layouts and dashboard

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -46,11 +46,10 @@
         </div>
     </nav>    
 
-    <div class="container">
-        <h3 class="text-center mt-3 mb-3">Laravel 10 Custom User Registration & Login</h3>
-        @yield('content')
+ 
+    @yield('content')
         
-    </div>
+
        
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
 </body>
